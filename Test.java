2de302dbc8ab870cd0c5f26fa675e15e8f49e834
bbk-pij-2014-76 <Syed.maqsood.ali@live.com>
2014-11-29 12:41:51 +0000

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test
-{
-	public static void main ( String args [] )
-	{
-		System.out.println ( " Hello World ");
-		System.out.println ( " I am MSc student ");
-		System.out.println ( " my course is Computer Science ");
-	}
-}
