faf5ecfe56f705b7b0fc8e168117ad349b49a394
BBK-PIJ-2014-76 <syed.maqsood.ali@live.com>
2014-11-29 11:57:26 +0000

    Helloooo

---
+++ Person.java
@@ -0,0 +1,34 @@
+public class Person {
+	public String name;
+	public Person next;
+	public Person prev;
+
+	public Person(String str)
+	{
+		this.name = str;
+		next = null;
+		prev = null;
+	}
+
+	public void add(Person newPerson)
+	{
+		if ( next == null ) {
+			next = newPerson;
+			prev = newPerson;
+		} else {
+			prev = newPerson;
+		}
+	}
+
+	public Person remove()
+	{
+		if ( prev != null ) {
+			 Person result = next;
+			 return result;
+		} else {
+			return null;
+		}
+
+	}
+
+}
