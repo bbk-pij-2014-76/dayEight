9cd824a5a45d6d3d9444e71ba8dd92ed3894ba42
BBK-PIJ-2014-76 <syed.maqsood.ali@live.com>
2014-11-29 12:11:33 +0000

    name-deleted

--- Test.java
+++ Test.java
@@ -3,7 +3,6 @@ public class Test
 	public static void main ( String args [] )
 	{
 		System.out.println ( " Hello World ");
-		System.out.println ( " my name is syed ");
 		System.out.println ( " I am MSc student ");
 	}
 }
